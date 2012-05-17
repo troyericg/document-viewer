4dd040b966f8c7e2dad0bcf28199069cd7fa94fb
Ted Han <ted@knowtheory.net>
2012-05-17 09:59:15 -0500

    Adding link to jump from partial list of comments in Document view to full list in Annotation view.

--- public/javascripts/DV/backbone/views/comment_list.js
+++ public/javascripts/DV/backbone/views/comment_list.js
@@ -1,16 +1,22 @@
 DV.backbone.view.CommentList = Backbone.View.extend({
   id: 'DV-commentsList',
   className: 'DV-comments',
-  events: {'click .DV-add_comment': 'addComment'},
+  events: { 
+    'click .DV-add_comment': 'addComment',
+    'click .DV-all_comments': 'openAnnotationList'
+  },
 
   constructor: function(options) {
+    this.viewer     = options.viewer;
+    this.note       = options.note;
+    console.log(this.note.id);
     this.collection = options.collection;
     this.collection.bind('add', this.render, this);
   },
 
   render: function() {
     DV.jQuery(this.el).html( JST['comment_list']({
-      commentItems: this.collection.reduce(function(html, comment){ return html += JST['comment_item']({comment:comment}); }, '')
+      commentItems: this.collection.reduce(function(html, comment){ return html += JST['comment_item']({comment:comment}); })
     }));
   },
 
@@ -18,5 +24,12 @@ DV.backbone.view.CommentList = Backbone.View.extend({
     var commentText = DV.jQuery(this.el).find('.DV-comment_input').val();
     DV.jQuery(this.el).find('.DV-comment_input').val('');
     this.collection.create( { commenter: DV.account.name, avatarUrl: DV.account.avatarUrl, text: commentText } );
+  },
+  
+  openAnnotationList: function() {
+    this.viewer.activeAnnotationId = this.note.id;
+    console.log("Opening Annotation list to: " + this.note.id)
+    console.log("Viewer activeAnnotationId: "+this.viewer.activeAnnotationId);
+    this.viewer.open('ViewAnnotation');
   }
 });
