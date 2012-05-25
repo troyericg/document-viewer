97bbe289d0679b8be5159c18367d5d3f1a6aa84e
Ted Han <ted@knowtheory.net>
2012-05-25 11:08:46 -0500

    Making sure accounts and the backbone initializer are included properly into test pages.

--- public/javascripts/DV/backbone/backbone_initalizer.js
+++
@@ -1,3 +0,0 @@
-// mock account object
-DV.account  = {name: 'Ted Han', avatarUrl: 'https://si0.twimg.com/profile_images/2187833737/hat_shot_90ccw_normal.jpg'}
-Backbone.sync = function(){ return true };
---
+++ public/javascripts/DV/backbone/backbone_initializer.js
@@ -0,0 +1,3 @@
+// mock account object
+DV.account  = {name: 'Ted Han', avatar_url: 'https://si0.twimg.com/profile_images/2187833737/hat_shot_90ccw_normal.jpg'}
+Backbone.sync = function(){ return true };
