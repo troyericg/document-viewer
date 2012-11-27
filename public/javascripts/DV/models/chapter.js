f39d1650223d12f367c7565d56594ce8a8f4b5dd
Ted Han <ted@knowtheory.net>
2012-11-26 21:47:07 -0600

    Annotating the chapters presenter.

--- public/javascripts/DV/models/chapter.js
+++ public/javascripts/DV/models/chapter.js
@@ -1,3 +1,6 @@
+// The Chapters view keep track of a document's
+// sections/chapters and on which page each
+// chapter begins.
 DV.model.Chapters = function(viewer) {
   this.viewer = viewer;
   this.loadChapters();
@@ -9,17 +12,36 @@ DV.model.Chapters.prototype = {
   loadChapters : function() {
     var sections = this.viewer.schema.data.sections;
     var chapters = this.chapters = this.viewer.schema.data.chapters = [];
+
+    if (sections.length < 1) return; // short circuit if there are no sections
+
+    // ensure each section has a unique id we can reference.
     _.each(sections, function(sec){ sec.id || (sec.id = _.uniqueId()); });
 
+    // Run through each page.
     var sectionIndex = 0;
-    for (var i = 0, l = this.viewer.schema.data.totalPages; i < l; i++) {
+    for (var i = 0; i < this.viewer.schema.data.totalPages; i++) {
+      
+      // N.B. section ids start with 1, not 0.
+      // thus the first section will be undefined.
+
+      // get the current and next sections.
       var section = sections[sectionIndex];
       var nextSection = sections[sectionIndex + 1];
+      
+      // Check whether the next section's page index
+      // is greater than or equal to the current page.
       if (nextSection && (i >= (nextSection.page - 1))) {
+        // when that is the case, increment the section index
+        // and move the next section to the current section.
         sectionIndex += 1;
         section = nextSection;
       }
-      if (section && !(section.page > i + 1)) chapters[i] = section.id;
+
+      // assign the chapter for page i to the current section
+      // if the section's page is less than or equal to the current
+      // page.
+      if (section && (section.page <= i + 1)) chapters[i] = section.id;
     }
   },
 
