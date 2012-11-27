73cf228bd3883e7de946fb94874ae6cc8b425fd8
Ted Han <ted@knowtheory.net>
2012-11-27 12:28:32 -0600

    Grab chapter model from other branch.

--- public/javascripts/DV/models/chapter.js
+++ public/javascripts/DV/models/chapter.js
@@ -1,3 +1,6 @@
+// The Chapters view keep track of a document's
+// sections/chapters and on which page each
+// chapter begins.
 DV.model.Chapters = function(viewer) {
   this.viewer = viewer;
   this.loadChapters();
@@ -6,30 +9,32 @@ DV.model.Chapters = function(viewer) {
 DV.model.Chapters.prototype = {
 
   // Load (or reload) the chapter model from the schema's defined sections.
-  loadChapters : function() {
+  loadChapters: function() {
     var sections = this.viewer.schema.data.sections;
     var chapters = this.chapters = this.viewer.schema.data.chapters = [];
-    _.each(sections, function(sec){ sec.id || (sec.id = _.uniqueId()); });
+    var pageCount = this.viewer.schema.data.totalPages;
 
-    var sectionIndex = 0;
-    for (var i = 0, l = this.viewer.schema.data.totalPages; i < l; i++) {
-      var section = sections[sectionIndex];
-      var nextSection = sections[sectionIndex + 1];
-      if (nextSection && (i >= (nextSection.page - 1))) {
-        sectionIndex += 1;
-        section = nextSection;
+    if (sections.length < 1) return; // short circuit if there are no sections
+    _.each(sections, function(section) {
+      // make sure each section has a unique id we can reference.
+      section.id || (section.id = _.uniqueId());
+
+      // so long as the pageIndex for the section is in bounds
+      // assign the section to the appropriate page's chapter.
+      var pageIndex = section.page - 1;
+      if (pageIndex >= 0 && pageIndex < pageCount) {
+        chapters[pageIndex] = section.id;
       }
-      if (section && !(section.page > i + 1)) chapters[i] = section.id;
-    }
+    });
   },
 
-  getChapterId: function(index){
+  getChapterId: function(index) {
     return this.chapters[index];
   },
 
-  getChapterPosition: function(chapterId){
-    for(var i = 0,len=this.chapters.length; i < len; i++){
-      if(this.chapters[i] === chapterId){
+  getChapterPosition: function(chapterId) {
+    for (var i = 0, len = this.chapters.length; i < len; i++) {
+      if (this.chapters[i] === chapterId) {
         return i;
       }
     }
