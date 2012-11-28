fce79cc8b5a9859fc2521bcaf3c7a6814921e23c
Ted Han <ted@knowtheory.net>
2012-11-28 06:30:11 -0600

    A little bit of cleanup on the navigation handler.

--- public/javascripts/DV/events/navigation.js
+++ public/javascripts/DV/events/navigation.js
@@ -7,43 +7,36 @@ _.extend(DV.Schema.events, {
     var chapterEl   = el.closest('.DV-chapter');          // Search for the nearest chapter link
     if (!triggerEl.length) return;                        // If you didn't click on anything we care about, bail.
 
-    if (el.hasClass('DV-expander')) {                                       // if the click target is a section collapsing arrow
-      return chapterEl.toggleClass('DV-collapsed');                         // collapse the section
-    } else if (noteEl.length) {                                             // otherwise if there is a note link nearby
+    if (noteEl.length) {                                                    // If there is a note link nearby
       var aid         = noteEl[0].id.replace('DV-annotationMarker-','');    // find the note's id
       var annotation  = this.viewer.models.annotations.getAnnotation(aid);  // get the note model
-      var pageNumber  = parseInt(annotation.index,10)+1;                    
 
-      if(this.viewer.state === 'ViewText'){                 // If we're vewing the text, load the text for this note's page.
-        this.loadText(annotation.index);
-
-        // this.viewer.history.save('text/p'+pageNumber);
-      }else{                                                // Otherwise show the note.
-        if (this.viewer.state === 'ViewThumbnails') {       // If we're looking at the page thumbnails
-          this.viewer.open('ViewDocument');                 // Open the document view first.
-        }
-        this.viewer.pageSet.showAnnotation(annotation);     // open!
+      // If we're vewing the text, load the text for this note's page.
+      if ( this.viewer.state === 'ViewText' ){ 
+        this.loadText(annotation.index); 
+      } else {
+        if (this.viewer.state === 'ViewThumbnails') { this.viewer.open('ViewDocument'); }
+        this.viewer.pageSet.showAnnotation(annotation); // Otherwise show the note.
       }
 
     } else if (chapterEl.length) {                                                            // If we have a chapter header
-      // its a header, take it to the page
-      chapterEl.removeClass('DV-collapsed');                                                  // Expand the section if necessary
-      var cid           = parseInt(chapterEl[0].id.replace('DV-chapter-',''), 10);            // chapter id
-      var chapterIndex  = parseInt(this.viewer.models.chapters.getChapterPosition(cid),10);   // Figure out the page index
-      var pageNumber    = parseInt(chapterIndex,10)+1;                                        // and page number
+      if (el.hasClass('DV-expander')) {                                                       // if the click target is a section collapsing arrow
+        return chapterEl.toggleClass('DV-collapsed');                                         // collapse the section and bail.
+      } else {
+        // its a header, take it to the page
+        chapterEl.removeClass('DV-collapsed');                                                  // Expand the section if necessary
+        var cid           = parseInt(chapterEl[0].id.replace('DV-chapter-',''), 10);            // chapter id
+        var chapterIndex  = parseInt(this.viewer.models.chapters.getChapterPosition(cid),10);   // Figure out the page index
 
-      if(this.viewer.state === 'ViewText'){                     // If we're viewing the text
-        this.loadText(chapterIndex);                            // load the appropriate page text
-        // this.viewer.history.save('text/p'+pageNumber);
-      }else if(this.viewer.state === 'ViewDocument' ||          // Otherwise if we're vewing the document
-               this.viewer.state === 'ViewThumbnails'){         // or thumbnails
-        this.helpers.jump(chapterIndex);                        // Jump to the appropriate page
-        // this.viewer.history.save('document/p'+pageNumber);
-        if (this.viewer.state === 'ViewThumbnails') {           // If we're viewing the thumbnails
-          this.viewer.open('ViewDocument');                     // we should open the document view
+        if(this.viewer.state === 'ViewText'){                     // If we're viewing the text
+          this.loadText(chapterIndex);                            // load the appropriate page text
+        } else if (this.viewer.state === 'ViewDocument' ||        // Otherwise if we're vewing the document
+                   this.viewer.state === 'ViewThumbnails'){       // or thumbnails
+          this.helpers.jump(chapterIndex);                        // Jump to the appropriate page
+          if (this.viewer.state === 'ViewThumbnails') { this.viewer.open('ViewDocument'); }
+        }else{                                                    // Otherwise (say in the note view)
+          return false;                                           // dunno lol.
         }
-      }else{                                                    // Otherwise (say in the note view)
-        return false;                                           // dunno lol.
       }
 
     }else{
