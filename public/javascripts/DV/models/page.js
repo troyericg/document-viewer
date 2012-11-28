c0cb0532af7151804a6f3e58350d000a1bb83863
Ted Han <ted@knowtheory.net>
2012-11-28 13:13:59 -0600

    Fix resources reference.

--- public/javascripts/DV/models/page.js
+++ public/javascripts/DV/models/page.js
@@ -40,10 +40,11 @@ DV.model.Pages.prototype = {
 
   // Get the complete image URL for a particular page.
   imageURL: function(index) {
-    var url  = this.viewer.schema.document.resources.page.image;
+    var resources = this.viewer.model.get('resources');
+    var url  = resources.page.image;
     var size = this.zoomLevel > this.BASE_WIDTH ? 'large' : 'normal';
     var pageNumber = index + 1;
-    if (this.viewer.schema.document.resources.page.zeropad) pageNumber = this.zeroPad(pageNumber, 5);
+    if (resources.page.zeropad) pageNumber = this.zeroPad(pageNumber, 5);
     url = url.replace(/\{size\}/, size);
     url = url.replace(/\{page\}/, pageNumber);
     return url;
