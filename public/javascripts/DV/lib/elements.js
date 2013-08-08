a4ab898d56400d1d9b30a27f13a0632f4864c34c
Ted Han <ted@knowtheory.net>
2013-08-08 18:49:39 -0500

    Moar _.each refactoring

--- public/javascripts/DV/lib/elements.js
+++ public/javascripts/DV/lib/elements.js
@@ -1,9 +1,6 @@
 DV.Elements = function(viewer){
   this._viewer = viewer;
-  var elements = DV.Schema.elements;
-  for (var i=0, elemCount=elements.length; i < elemCount; i++) {
-    this.getElement(elements[i]);
-  }
+  _.each(DV.Schema.elements, function(element){ this.getElement(element); }, this);
 };
 
 // Get and store an element reference
